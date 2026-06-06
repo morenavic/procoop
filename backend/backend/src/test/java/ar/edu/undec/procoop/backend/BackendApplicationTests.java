55a56cae06d3035422c5e86f77534ce6e1070858
Morena Gallardo <morevg.996@gmail.com>
2026-06-06 08:01:16 -0300

    Setup Inicial del Proyecto.

---
+++ backend/backend/src/test/java/ar/edu/undec/procoop/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.procoop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/src/main/java/ar/edu/undec/procoop/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.procoop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
