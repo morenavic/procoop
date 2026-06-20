8c457084a4af23fc0f973588fac8232c10b881dd
Morena Gallardo <morevg.996@gmail.com>
2026-06-20 07:32:17 -0300

    Refactorización de  backend:
    - Se eliminó carpeta backend duplicada.
    - Agregar nuevos scripts de inicialización de la base de datos
    - Actualizar los datos de intert.sql para roles, novedades, usuarios, productos, servicios y documentos.

--- backend/src/main/java/ar/edu/undec/procoop/backend/BackendApplication.java
+++ backend/src/main/java/ar/edu/undec/procoop/backend/BackendApplication.java
@@ -2,11 +2,15 @@ package ar.edu.undec.procoop.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+//		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//		System.out.println("Clientes123!: " + encoder.encode("Clientes123!"));
+//		System.out.println("Admin123!: " + encoder.encode("Admin123!"));
 	}
 }
